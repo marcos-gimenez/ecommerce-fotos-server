2f4bb9560346263540d5cc26f376a1ef405bb611
Marcos <marcos.s.g1989@gmail.com>
2026-01-02 12:07:57 -0300

    marca de agua 7

--- utils/cloudinaryPreview.js
+++ utils/cloudinaryPreview.js
@@ -1,3 +1,29 @@
+// import cloudinary from '../config/cloudinary.js';
+
+// export function getPreviewUrl(media) {
+//   return cloudinary.url(media.public_id, {
+//     resource_type: 'image',
+//     transformation: [
+//       // 1 Imagen base
+//       { width: 1200, crop: 'limit', quality: 'auto:eco' },
+
+//       // 2 Blur fuerte (preview protegida)
+//       { effect: 'blur:150' },
+
+//       // 3 Watermark
+//       {
+//         overlay: 'image:watermark_pampa_foto',
+//         gravity: 'center',
+//         opacity: 80,
+//         width: 650,
+//       },
+
+//       // 4 Aplicar overlay
+//       { flags: 'layer_apply' },
+//     ],
+//   });
+// }
+
 import cloudinary from '../config/cloudinary.js';
 
 export function getPreviewUrl(media) {
@@ -10,16 +36,20 @@ export function getPreviewUrl(media) {
       // 2️⃣ Blur fuerte (preview protegida)
       { effect: 'blur:150' },
 
-      // 3️⃣ Watermark
+      // 3️⃣ Watermark ocupando todo el ancho
       {
         overlay: 'image:watermark_pampa_foto',
+        width: '1.0',
+        crop: 'scale',
         gravity: 'center',
         opacity: 90,
-        width: 950,
       },
 
       // 4️⃣ Aplicar overlay
       { flags: 'layer_apply' },
+
+      // 5️⃣ Oscurecer levemente la imagen final
+      { effect: 'brightness:-10' },
     ],
   });
 }
